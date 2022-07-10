24a80685d001cea797d3de18c04490a66e66b647
amitdor <amit@particula-tech.com>
2022-07-10 10:11:00 +0300

    [+] Added support for DnD Shells [+] More accurate calculation of die outcome

--- godice.js
+++ godice.js
@@ -19,13 +19,214 @@ class GoDice {
 		YELLOW: 4,
 		ORANGE: 5,
 	}
-
+	
+	static diceTypes = {
+		D6: 0,
+		D20: 1,
+		D10: 2,
+		D10X: 3,
+		D4: 4,
+		D8: 5,
+		D12: 6,
+	}
+	
+	// The vectors for each die and shell type, used to check which side is facing up
+	static d6Vectors = {
+		1: [-64, 0, 0],
+		2: [0, 0, 64],
+		3: [0, 64, 0],
+		4: [0, -64, 0],
+		5: [0, 0, -64],
+		6: [64, 0, 0],
+	}
+	
+	static d20Vectors = {
+		1: [-64, 0, -22],
+		2: [42, -42, 40],
+		3: [0, 22, -64],
+		4: [0, 22, 64],
+		5: [-42, -42, 42],
+		6: [22, 64, 0],
+		7: [-42, -42, -42],
+		8: [64, 0, -22],
+		9: [-22, 64, 0],
+		10: [42, -42, -42],
+		11: [-42, 42, 42],
+		12: [22, -64, 0],
+		13: [-64, 0, 22],
+		14: [42, 42, 42],
+		15: [-22, -64, 0],
+		16: [42, 42, -42],
+		17: [0, -22, -64],
+		18: [0, -22, 64],
+		19: [-42, 42, -42],
+		20: [64, 0, 22],
+	}
+	
+	static d24Vectors = {
+		1: [20, -60, -20],
+		2: [20, 0, 60],
+		3: [-40, -40, 40],
+		4: [-60, 0, 20],
+		5: [40, 20, 40],
+		6: [-20, -60, -20],
+		7: [20, 60, 20],
+		8: [-40, 20, -40],
+		9: [-40, 40, 40],
+		10: [-20, 0, 60],
+		11: [-20, -60, 20],
+		12: [60, 0, 20],
+		13: [-60, 0, -20],
+		14: [20, 60, -20],
+		15: [20, 0, -60],
+		16: [40, -20, -40],
+		17: [-20, 60, -20],
+		18: [-40, -40, -40],
+		19: [40, -20, 40],
+		20: [20, -60, 20],
+		21: [60, 0, -20],
+		22: [40, 20, -40],
+		23: [-20, 0, -60],
+		24: [-20, 60, 20],
+	}
+	
+	// Transforms from each shell type to according number on shell
+	// D20 Transforms
+	static d10Transform = {
+		1: 8,
+		2: 2,
+		3: 6,
+		4: 1,
+		5: 4,
+		6: 3,
+		7: 9,
+		8: 0,
+		9: 7,
+		10: 5,
+		11: 5,
+		12: 7,
+		13: 0,
+		14: 9,
+		15: 3,
+		16: 4,
+		17: 1,
+		18: 6,
+		19: 2,
+		20: 8,
+	}
+	
+	static d10XTransform = {
+		1: 80,
+		2: 20,
+		3: 60,
+		4: 10,
+		5: 40,
+		6: 30,
+		7: 90,
+		8: 0,
+		9: 70,
+		10: 50,
+		11: 50,
+		12: 70,
+		13: 0,
+		14: 90,
+		15: 30,
+		16: 40,
+		17: 10,
+		18: 60,
+		19: 20,
+		20: 80,
+	}
+	
+	// D24 Transforms
+	static d4Transform = {
+		1: 3,
+		2: 1,
+		3: 4,
+		4: 1,
+		5: 4,
+		6: 4,
+		7: 1,
+		8: 4,
+		9: 2,
+		10: 3,
+		11: 1,
+		12: 1,
+		13: 1,
+		14: 4,
+		15: 2,
+		16: 3,
+		17: 3,
+		18: 2,
+		19: 2,
+		20: 2,
+		21: 4,
+		22: 1,
+		23: 3,
+		24: 2,
+	}
+	
+	static d8Transform = {
+		1: 3,
+		2: 3,
+		3: 6,
+		4: 1,
+		5: 2,
+		6: 8,
+		7: 1,
+		8: 1,
+		9: 4,
+		10: 7,
+		11: 5,
+		12: 5,
+		13: 4,
+		14: 4,
+		15: 2,
+		16: 5,
+		17: 7,
+		18: 7,
+		19: 8,
+		20: 2,
+		21: 8,
+		22: 3,
+		23: 6,
+		24: 6,
+	}
+	
+	static d12Transform = {
+		1: 1,
+		2: 2,
+		3: 3,
+		4: 4,
+		5: 5,
+		6: 6,
+		7: 7,
+		8: 8,
+		9: 9,
+		10: 10,
+		11: 11,
+		12: 12,
+		13: 1,
+		14: 2,
+		15: 3,
+		16: 4,
+		17: 5,
+		18: 6,
+		19: 7,
+		20: 8,
+		21: 9,
+		22: 10,
+		23: 11,
+		24: 12,
+	}
+	
 	bluetoothDevice;
 	GoDiceService;
 	CubeCharacteristics;
 	GlobalDeviceId;
 	diceId;
 	rolledValue = 0;
+	dieType = GoDice.diceTypes.D6; // Type of die
 
 	onRollStart(){};
 	onBatteryLevel(){};
@@ -53,7 +254,12 @@ class GoDice {
 		console.log(this)
 		this.sendMessage([this.messageIdentifiers.DICE_COLOUR]);
 	}
-
+	
+	setDieType(newDieType) {
+		this.dieType = newDieType
+	}
+	
+	
 	/**
 	 * Open a connection dialog to connect a single GoDice, after successfull connection it will follow by corresponding "DiceConnected" event (response).
 	 */
@@ -107,56 +313,110 @@ class GoDice {
 				});
 			});
 	}
-
-	// Change angles to fixed value
-	getAngleValue(val) {
-		if (val <= 20 && val >= 0) {
-			return "255";
+	
+	/**
+	* Calculates the closest vector from the table to the given coord
+	* @param {dictionary} DieTable - Vector table of current shell
+	* @param {dictionary} coord - Vector of die
+	*
+	**/
+	getClosestVector(DieTable, coord) {
+		
+		const coordX = coord[0]
+		const coordY = coord[1]
+		const coordZ = coord[2]
+				
+		var minDistance = Number.MAX_SAFE_INTEGER
+		var value = 0
+		var vector = []
+		var XResult = 0
+		var YResult = 0
+		var ZResult = 0
+		var curDist = 0
+		
+		// Calculating distance to each value in vector array
+		for (let dieValue in DieTable) 
+		{	
+			vector = DieTable[dieValue]
+			
+			XResult = coordX - vector[0]
+			YResult = coordY - vector[1]
+			ZResult = coordZ - vector[2]
+			
+			// Calculating squared magnitude (since it's only for comparing there's no need for sqrt)
+			curDist = ((XResult ** 2) + (YResult ** 2) + (ZResult ** 2))
+			
+			if (curDist < minDistance) {
+				minDistance = curDist
+				value = dieValue
+			}
+			
 		}
-		if (val <= 255 && val >= 230) {
-			return "255";
-		}
-		if (val <= 85 && val >= 40) {
-			return "64";
-		}
-		if (val <= 215 && val >= 185) {
-			return "192";
-		}
-
+		return value
 	}
-
+	
+	// Gets the xyz coord from sent message
 	getXyzFromBytes(data, startByte) {
-		const x = data.getUint8(startByte);
-		const y = data.getUint8(startByte + 1);
-		const z = data.getUint8(startByte + 2);
+		const x = data.getInt8(startByte);
+		const y = data.getInt8(startByte + 1);
+		const z = data.getInt8(startByte + 2);
 		return [x, y, z]
 	}
 
-	// Get D6 number from acc raw data
-	getD6Value(data, startByte) {
+	// Get Die number from acc raw data according to die type
+	getDieValue(data, startByte) {
+		
 		const xyzArray = this.getXyzFromBytes(data, startByte)
-		const coord = `${this.getAngleValue(xyzArray[0])}-${this.getAngleValue(xyzArray[1])}-${this.getAngleValue(xyzArray[2])}`;
-		switch (coord) {
-			case ("192-255-255"):
-				return 1;
-			case ("255-255-64"):
-				return 2;
-			case ("255-64-255"):
-				return 3;
-			case ("255-192-255"):
-				return 4;
-			case ("255-255-192"):
-				return 5;
-			case ("64-255-255"):
-				return 6;
-
-			default:
-				//сonsole.log("no hit coord",coord);
-				return "";
+		var value = 0;
+		
+		// Calculating closest vector according to current die type
+		if (this.dieType == GoDice.diceTypes.D6){
+			value = this.getClosestVector(GoDice.d6Vectors, xyzArray)
 		}
-	}
+		else if (this.dieType >= GoDice.diceTypes.D20 && this.dieType <= GoDice.diceTypes.D10X) {
+			// D20 shell type
+			value = this.getClosestVector(GoDice.d20Vectors, xyzArray)
+			
+			switch (this.dieType) {
+				case (GoDice.diceTypes.D10):
+					value = GoDice.d10Transform[value]
+					break
+				case (GoDice.diceTypes.D10X):
+					value = GoDice.d10XTransform[value]
+					break
+			}
+		}
+		else if (this.dieType >= GoDice.diceTypes.D4 && this.dieType <= GoDice.diceTypes.D12){
+			value = this.getClosestVector(GoDice.d24Vectors, xyzArray)
+			
+			// D24 shell type
+			switch (this.dieType) {
+				case (GoDice.diceTypes.D4):
+					value = GoDice.d4Transform[value]
+					break
+				case (GoDice.diceTypes.D8):
+					value = GoDice.d8Transform[value]
+					break
+				case (GoDice.diceTypes.D12):
+					value = GoDice.d12Transform[value]
+					break
+			}
+		}
+		return value
 
-	// Get a message fromn the die and fire the matchig event
+	}
+	
+	getD20Value (data, startByte) {
+		const xyzArray = this.getXyzFromBytes(data, startByte)
+		return this.getClosestVector(this.d20Vectors, xyzArray)
+	}
+	
+	getD24Value (data, startByte) {
+		const xyzArray = this.getXyzFromBytes(data, startByte)
+		return this.getClosestVector(this.d24Vectors, xyzArray)
+	}
+	
+	// Get a message fromn the die and fire the matching event
 	parseMessage(data, deviceId) {
 		try {
 			console.debug("data: ", data);
@@ -179,28 +439,29 @@ class GoDice {
 			}
 
 			if (firstByte === 83) {
-				const diceCurrentNumber = this.getD6Value(data, 1);
+				const diceCurrentNumber = this.getDieValue(data, 1);
 				const xyzArray = this.getXyzFromBytes(data, 1)
 				this.rolledValue = diceCurrentNumber;
-				if (parseInt(diceCurrentNumber) > 0) {
-					this.onStable(deviceId, diceCurrentNumber, xyzArray);
+				this.onStable(deviceId, diceCurrentNumber, xyzArray);
 				}
 			}
 
 			if (firstByte === 70 && secondByte === 83) {
-				const diceCurrentNumber = this.getD6Value(data, 2);
+				const diceCurrentNumber = this.getDieValue(data, 2);
 				const xyzArray = this.getXyzFromBytes(data, 2)
 				this.rolledValue = diceCurrentNumber;
 				this.onFakeStable(deviceId, diceCurrentNumber, xyzArray);
 			}
 
 			if (firstByte === 84 && secondByte === 83) {
+				const diceCurrentNumber = this.getDieValue(data, 2);
 				const xyzArray = this.getXyzFromBytes(data, 2)
-				this.onTiltStable(deviceId, xyzArray);
+				this.rolledValue = diceCurrentNumber;
+				this.onTiltStable(deviceId, xyzArray, diceCurrentNumber);
 			}
 
 			if (firstByte === 77 && secondByte === 83) {
-				const diceCurrentNumber = this.getD6Value(data, 2);
+				const diceCurrentNumber = this.getDieValue(data, 2);
 				const xyzArray = this.getXyzFromBytes(data, 2)
 				this.rolledValue = diceCurrentNumber;
 				this.onMoveStable(deviceId, diceCurrentNumber, xyzArray);
--- main.js
+++ main.js
@@ -20,7 +20,6 @@ function getDiceHtmlEl(diceID) {
 	return document.getElementById(diceID);
 }
 
-
 GoDice.prototype.onDiceConnected = (diceId, diceInstance) => {
 	console.log("Dice connected: ", diceId);
 	
@@ -29,7 +28,7 @@ GoDice.prototype.onDiceConnected = (diceId, diceInstance) => {
 
 	// get new die element or it's instance if it's already exists
 	const diceHtmlEl = getDiceHtmlEl(diceId);
-
+	
 	// clear existing die element content if it exists
 	while (diceHtmlEl.firstChild) {
         	diceHtmlEl.removeChild(diceHtmlEl.lastChild);
@@ -79,6 +78,52 @@ GoDice.prototype.onDiceConnected = (diceId, diceInstance) => {
 	getDiceColorButton.onclick = diceInstance.getDiceColor.bind(diceInstance)
 	getDiceColorButton.textContent = 'Get Dice Color';
 	diceHtmlEl.append(getDiceColorButton)
+	
+	// Change die type buttons select
+	const d6Button = document.createElement('button');
+	d6Button.className = 'diebtn btn-outline-primary';
+	d6Button.onclick = diceInstance.setDieType.bind(diceInstance, GoDice.diceTypes.D6)
+	d6Button.textContent = 'D6';
+	diceHtmlEl.append(d6Button)
+	
+	// D20 Shell
+	const d20Button = document.createElement('button');
+	d20Button.className = 'diebtn btn-outline-primary';
+	d20Button.onclick = diceInstance.setDieType.bind(diceInstance, GoDice.diceTypes.D20)
+	d20Button.textContent = 'D20';
+	diceHtmlEl.append(d20Button)
+	
+	const d10Button = document.createElement('button');
+	d10Button.className = 'diebtn btn-outline-primary';
+	d10Button.onclick = diceInstance.setDieType.bind(diceInstance, GoDice.diceTypes.D10)
+	d10Button.textContent = 'D10';
+	diceHtmlEl.append(d10Button)
+	
+	const d10XButton = document.createElement('button');
+	d10XButton.className = 'diebtn btn-outline-primary';
+	d10XButton.onclick = diceInstance.setDieType.bind(diceInstance, GoDice.diceTypes.D10X)
+	d10XButton.textContent = 'D10X';
+	diceHtmlEl.append(d10XButton)
+	
+	// D24 Shell
+	const d4Button = document.createElement('button');
+	d4Button.className = 'diebtn btn-outline-primary';
+	d4Button.onclick = diceInstance.setDieType.bind(diceInstance, GoDice.diceTypes.D4)
+	d4Button.textContent = 'D4';
+	diceHtmlEl.append(d4Button)
+	
+	const d8Button = document.createElement('button');
+	d8Button.className = 'diebtn btn-outline-primary';
+	d8Button.onclick = diceInstance.setDieType.bind(diceInstance, GoDice.diceTypes.D8)
+	d8Button.textContent = 'D8';
+	diceHtmlEl.append(d8Button)
+	
+	const d12Button = document.createElement('button');
+	d12Button.className = 'diebtn btn-outline-primary';
+	d12Button.onclick = diceInstance.setDieType.bind(diceInstance, GoDice.diceTypes.D12)
+	d12Button.textContent = 'D12';
+	diceHtmlEl.append(d12Button)
+	
 
 	// add battery level indicator
 	const colorIndicator = document.createElement('div');
@@ -113,12 +158,12 @@ GoDice.prototype.onStable = (diceId, value, xyzArray) => {
 	diceIndicatorEl.textContent = "Stable: " + value;
 };
 
-GoDice.prototype.onTiltStable = (diceId, xyzArray) => {
+GoDice.prototype.onTiltStable = (diceId, xyzArray, value) => {
 	console.log("TiltStable: ", diceId, xyzArray);
 
 	// Get tile indicator and show raw data
 	const diceIndicatorEl = document.getElementById(diceId + "-die-status");
-	diceIndicatorEl.textContent = "Tilt Stable: x=" + xyzArray[0] + " ,y=" + xyzArray[1] + " ,z=" + xyzArray[2];
+	diceIndicatorEl.textContent = "Tilt Stable: value: " + value + " , x=" + xyzArray[0] + " ,y=" + xyzArray[1] + " ,z=" + xyzArray[2];
 };
 
 GoDice.prototype.onFakeStable = (diceId, value, xyzArray) => {
